7bd5cbed50ebd4ba195e6443184eb8d79f6dacac
Prem Manjhi <pk2049822@gmail.com>
2024-01-03 13:16:59 +0530

    Play

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const min=1;
 const max=100;
 const random=Math.floor(Math.random()*(max-min+1)+min);
+const warn=document.querySelector('#warning');
 
 const form=document.querySelector('form');
 let chance=10
@@ -17,30 +18,26 @@ function Chance(prev,chance){
 }
 form.addEventListener('submit',function(e) {
 
-    e.preventDefault();
-    const number=Number(document.querySelector("#number").value);
+e.preventDefault();
+const number=Number(document.querySelector("#number").value);
 
-console.log(number);
+// console.log(number);
 
     if(number<=0||number===''||isNaN(number)||number>100){
-        const warn=document.querySelector('#warning');
         warn.innerHTML="Oho !! Please Enter A Valid Number";
         // submit.style.display="none";
 
     }
     else{
     chance =chance-1;
-    // GuessNumber.push(number);
     Chance(number,chance);
         if(random===number){
-            const warn=document.querySelector('#warning');
             warn.style.color="green"
             warn.innerHTML="Hurry !!! You Won The Game";
             submit.style.display="none";
 
         }
         else if(chance===0){
-            const warn=document.querySelector('#warning');
             warn.style.color="red"
             warn.innerHTML="Oops!!! You Miss The Chance Refresh And Try Again The Actual Number Is :"+random;
             submit.style.display="none";
